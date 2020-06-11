efa637782c38ae46a4a2ae504a102df3a9daa3b4
Sylvain Gamot <>
2020-06-11 15:04:11 +0200

    Refactor test

--- test/test_kwalify_to_json_schema.rb
+++ test/test_kwalify_to_json_schema.rb
@@ -4,21 +4,23 @@ require_relative "../lib/kwalify_to_json_schema"
 
 module KwalifyToJsonSchema
   class Test < Minitest::Test
-    @@tmpdir = Dir.mktmpdir
     @@debug = false
+    @@tmpdir = Dir.mktmpdir
 
     # Create a test method for every Kwalify schema
     Dir.glob(File.join(__dir__, "schemas", "kwalify", "*.yaml")).each { |source|
       test_name_base = File.basename(source, File.extname(source))
+      expected_types = %w(json yaml)
 
-      %w(json yaml).map { |expected_type|
-
-        # Define a method for the test JSON output
-        define_method("test_#{test_name_base}_#{expected_type}_output".to_sym) {
+      # Define a method for the test JSON output
+      define_method("test_#{test_name_base}_output".to_sym) {
+        types_done = 0
+        expected_types.map { |expected_type|
           output_file = test_name_base + ".#{expected_type}"
           expected = File.join(File.join(__dir__, "schemas", "json_schema", expected_type, output_file))
 
-          skip "Expected #{expected_type.upcase} result does not exist for test #{test_name_base}. The #{expected} file is missing" unless File.exist?(expected)
+          next unless File.exist?(expected)
+          types_done += 1
 
           ser = KwalifyToJsonSchema::Serialization::serialization_for_type(expected_type)
           dest = File.join(@@tmpdir, output_file)
@@ -39,6 +41,8 @@ module KwalifyToJsonSchema
             ser.normalize(File.read(dest))
           )
         }
+
+        skip "None of the expected #{expected_types.join(", ")} result for test #{test_name_base} was found" if types_done == 0
       }
     }
 
