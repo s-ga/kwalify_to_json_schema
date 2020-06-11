bc4d1abf83a9447164cc876e76d37203d5d2f276
Sylvain Gamot <sylvain.gamot@gmail.com>
2020-06-11 21:27:47 +0200

    Add CLI issues_to_description option

--- lib/kwalify_to_json_schema/cli.rb
+++ lib/kwalify_to_json_schema/cli.rb
@@ -5,8 +5,13 @@ module KwalifyToJsonSchema
 
     desc "convert KWALIFY_SCHEMA_FILE, RESULT_FILE", "Convert a Kwalify schema file to a JSON schema file. The result file extension will decide the format: .json or .yaml"
 
+    method_option :issues_to_description => :boolean, :default => false, :desc => "Will append any conversion issue to the schema description"
+
     def convert(kwalify_schema_file, result_file)
-      KwalifyToJsonSchema.convert_file(kwalify_schema_file, result_file)
+      options = {
+        issues_to_description: issues_to_description?,
+      }
+      KwalifyToJsonSchema.convert_file(kwalify_schema_file, result_file, options)
     end
 
     def self.exit_on_failure?
