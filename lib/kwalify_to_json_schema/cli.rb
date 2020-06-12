a1d003a8dedd9a346cefc8be7dfb03cbe606d96b
Sylvain Gamot <>
2020-06-12 02:35:22 +0200

    Add recursive option

--- lib/kwalify_to_json_schema/cli.rb
+++ lib/kwalify_to_json_schema/cli.rb
@@ -15,12 +15,15 @@ module KwalifyToJsonSchema
     desc "convert_dir KWALIFY_SCHEMA_DIR, RESULT_DIR", "Convert all the Kwalify schema from a directory to a JSON schema"
     option :issues_to_description, :type => :boolean, :default => false, :desc => "Will append any conversion issue to the schema description"
     option :format, :type => :string, :enum => ["json", "yaml"], :default => "json", :desc => "Select the output file format"
+    option :recursive, :type => :boolean, :default => false, :desc => "Process files recursively"
 
     def convert_dir(kwalify_schema_dir, result_dir)
       opts = {
         issues_to_description: options[:issues_to_description],
       }
-      Dir.glob(File.join(kwalify_schema_dir, "*.yaml")).each { |kwalify_schema_file|
+
+      path = [kwalify_schema_dir, options["recursive"] ? "**" : nil, "*.yaml"].compact
+      Dir.glob(File.join(*path)).each { |kwalify_schema_file|
         result_file = File.join(result_dir, File.basename(kwalify_schema_file, File.extname(kwalify_schema_file))) + ".#{options["format"]}"
         KwalifyToJsonSchema.convert_file(kwalify_schema_file, result_file, opts)
       }
