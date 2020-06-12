5fe5f3f0ac43b143d96e97846e1f7b86138b6c71
Sylvain Gamot <sylvain.gamot@gmail.com>
2020-06-12 12:02:47 +0200

    Formatting

--- lib/kwalify_to_json_schema/cli.rb
+++ lib/kwalify_to_json_schema/cli.rb
@@ -1,10 +1,14 @@
 module KwalifyToJsonSchema
   class Cli < Thor
-    package_name "kwalify_to_json_schema"
 
-    desc "convert KWALIFY_SCHEMA_FILE, RESULT_FILE", "Convert a Kwalify schema file to a JSON schema file. The result file extension will decide the format: .json or .yaml"
-    option :issues_to_description, :type => :boolean, :default => false, :desc => "Will append any conversion issue to the schema description"
+    ###############################################################################################################
 
+    desc "convert KWALIFY_SCHEMA_FILE, RESULT_FILE",
+         "Convert a Kwalify schema file to a JSON schema file. The result file extension will decide the format: .json or .yaml"
+    option :issues_to_description,
+           :type => :boolean,
+           :default => false,
+           :desc => "Will append any conversion issue to the schema description"
     def convert(kwalify_schema_file, result_file)
       opts = {
         issues_to_description: options[:issues_to_description],
@@ -12,10 +16,23 @@ module KwalifyToJsonSchema
       KwalifyToJsonSchema.convert_file(kwalify_schema_file, result_file, opts)
     end
 
-    desc "convert_dir KWALIFY_SCHEMA_DIR, RESULT_DIR", "Convert all the Kwalify schema from a directory to a JSON schema"
-    option :issues_to_description, :type => :boolean, :default => false, :desc => "Will append any conversion issue to the schema description"
-    option :format, :type => :string, :enum => ["json", "yaml"], :default => "json", :desc => "Select the output file format"
-    option :recursive, :type => :boolean, :default => false, :desc => "Process files recursively"
+    ###############################################################################################################
+
+    desc "convert_dir KWALIFY_SCHEMA_DIR, RESULT_DIR",
+         "Convert all the Kwalify schema from a directory to a JSON schema"
+    option :issues_to_description,
+           :type => :boolean,
+           :default => false,
+           :desc => "Will append any conversion issue to the schema description"
+    option :format,
+           :type => :string,
+           :enum => ["json", "yaml"],
+           :default => "json",
+           :desc => "Select the output file format"
+    option :recursive,
+           :type => :boolean,
+           :default => false,
+           :desc => "Process files recursively"
 
     def convert_dir(kwalify_schema_dir, result_dir)
       opts = {
